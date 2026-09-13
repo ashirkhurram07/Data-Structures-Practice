1f8fbf90982b62641aef5024bae2aa6af1cc8c30
Ashir Khurram <ashirkhurram07@gmail.com>
2026-09-13 21:01:31 +0500

    Double Linked List

--- DoubleLinkedList.cpp
+++ DoubleLinkedList.cpp
@@ -62,6 +62,33 @@ class DLL
             tail=nn;
         }
     }
+    void insertAtLoc(int data,int loc)
+    {
+		Node* newNode=createNode(data);
+        if(loc==1)
+        {
+          insertAtHead(data);
+        }
+        else
+        {
+			Node* temp=head;
+            for(int i=1;i<loc&&temp!=tail;i++)
+			temp=temp->next;
+			if(temp!=tail)
+			{
+				newNode->next=temp;
+				newNode->prev=temp->prev;
+				newNode->prev->next=newNode;
+				newNode->next->prev=newNode;
+			}
+			if(temp==tail)
+			{
+				delete newNode;
+				insertAtTail(data);
+			}
+			else cout<<"Location doesn't exist"<<endl;
+        }
+    }
 };
 int main()
 {
