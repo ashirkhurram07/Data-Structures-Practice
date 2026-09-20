432defbad6e30c83217b7ec0dc1c04dd2f470ed9
Ashir Khurram <ashirkhurram07@gmail.com>
2026-09-20 17:05:01 +0500

    Removing every kth node

--- RemoveKthNode.cpp
+++ RemoveKthNode.cpp
@@ -1,27 +1,60 @@
-// This is only the function. To test it add it to the single linked list file.
+// This is only the function. To check its working add it to the single linked list 
 void removeKthNode(int k)
-{
-    Node* temp=head;
-    if(isEmpty())
     {
-        cout<<"List is empty."<<endl;
-        return;
-    }
-    if(temp->next==nullptr)
-    {
-        cout<<"Nothing to remove only 1 node."<<endl;
-        return;
-    }
-    int i=1;
-    while(temp!=nullptr)
-    {
-        if(k%i==0)
+        Node* temp=head;
+        if(isEmpty())
         {
-            Node* temp2=temp->next;
-            temp->next=temp2->next;
-            delete temp2;
+            cout<<"List is empty."<<endl;
+            return;
         }
-        temp=temp->next;
-        i++;
+        if(temp->next==nullptr)
+        {
+            cout<<"Nothing to remove only 1 node."<<endl;
+            return;
+        }
+        int i=1;
+        Node* previous=nullptr;
+        while(temp!=nullptr)
+        {
+            if(i%k==0)
+            {
+                Node* nodetodel=temp;
+                temp=temp->next;
+                if(previous==nullptr)
+                {
+                    head=temp;
+                }
+                else
+                {
+                    previous->next=temp;
+                }
+                delete nodetodel; 
+            }
+            else
+            {
+                previous=temp;
+                temp=temp->next;
+            }
+            i++;
+        }
     }
-}
+int main()
+{
+    Linkedlist ll;
+    ll.insertAtHead(15);
+    ll.insertAtHead(12);
+    ll.insertAtHead(13);
+    ll.insertAtHead(19);
+    ll.insertAtHead(20);
+    ll.insertAtHead(45);
+    ll.insertAtHead(15);
+    ll.insertAtHead(12);
+    ll.insertAtHead(13);
+    ll.insertAtHead(19);
+    ll.insertAtHead(20);
+    ll.insertAtHead(45);
+    ll.display();
+    cout<<endl;
+    ll.removeKthNode(3);
+    ll.display();
+} 
