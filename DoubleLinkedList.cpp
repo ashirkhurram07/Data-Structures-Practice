0e30dc2462150461a967ecf77abd44ae3f0e5c93
Ashir Khurram <ashirkhurram07@gmail.com>
2026-09-21 23:44:33 +0500

    Added some things

--- DoubleLinkedList.cpp
+++ DoubleLinkedList.cpp
@@ -280,28 +280,135 @@ class DLL
             cout<<"NULL"<<endl;
         }
     }
+    void deleteBetween(int first,int second)
+    {
+        Node* current=head;
+        while(current->data!=first&&current!=nullptr)
+        current=current->next;
+        if(current==nullptr)
+        {
+            cout<<"First value not found."<<endl;
+            return;
+        }
+        Node* firstNode=current;
+        while(current->data!=second&&current!=tail)
+        current=current->next;
+        if(current->data!=second)
+        {
+            cout<<"Second value not found"<<endl;
+            return;
+        }
+        Node* secondNode=current;
+        while(firstNode->next!=secondNode)
+        {
+            Node* temp=firstNode->next;
+            firstNode->next=firstNode->next->next;
+            firstNode->next->prev=firstNode;
+            delete temp;
+        }
+    }
+
+    void swapNodes(int val1,int val2)
+    {
+        Node* temp=head;
+        Node* first=nullptr;
+        Node* second=nullptr;
+        if(val1==val2)
+        {
+            return;
+        }
+        while(temp!=nullptr)
+        {
+            if(temp->data==val1&&first==nullptr)
+            {
+                first=temp;
+            }
+            if(temp->data==val2&&second==nullptr)
+            {
+                second=temp;
+            }
+            temp=temp->next;
+        }
+        if(first==nullptr||second==nullptr)
+        {
+            cout<<"One or both values not found."<<endl;
+            return;
+        }
+        if(second->next==first)
+        {
+            Node* swap=second;
+            second=first;
+            first=swap;
+        }
+
+        if(first->next==second)
+        {
+            Node* beforeFirst=first->prev;
+            Node* afterSecond=second->next;
+            if(beforeFirst!=nullptr)
+            beforeFirst->next=second;
+            else
+            head=second;
+
+            if(afterSecond!=nullptr)
+            afterSecond->prev=first;
+            else
+            tail=first;
+
+            second->prev=beforeFirst;
+            second->next=first;
+
+            first->prev=second;
+            first->next=afterSecond;
+
+            return;                                     
+        }
+
+        Node* firstNext=first->next;
+        Node* firstPrev=first->prev;
+        Node* secondNext=second->next;
+        Node* secondPrev=second->prev;
+
+        if(firstPrev!=nullptr)
+        firstPrev->next=second;
+        else
+        head=second;
+
+        if(firstNext!=nullptr)
+        firstNext->prev=second;
+        else
+        tail=second;
+
+
+        if(secondPrev!=nullptr)
+        secondPrev->next=first;
+        else
+        head=first;
+
+        if(secondNext!=nullptr)
+        secondNext->prev=first;
+        else 
+        tail=first;
+
+        first->prev=secondPrev;
+        first->next=secondNext;
+
+        second->prev=firstPrev;
+        second->next=firstNext;
+        
+    }
 };
 int main()
 {
     DLL dll;
     dll.display();
-    dll.insertAtHead(10);
-    dll.display();
+    dll.insertAtTail(10);
     dll.insertAtTail(20);
+    dll.insertAtTail(30);
+    dll.insertAtTail(40);
+    dll.insertAtTail(50);
+    dll.insertAtTail(60);
     dll.display();
-    dll.insertAfterValue(30,20);
+    dll.swapNodes(50,20);
     dll.display();
-    dll.insertBeforeValue(15,20);
-    dll.display();
-    dll.insertAtLoc(50,5);
-    dll.display();
-    dll.deleteAtHead();
-    dll.display();
-    dll.deleteAtTail();
-    dll.display();
-    dll.deleteAtLoc(3);
-    dll.display();
-    dll.deleteAtVal(20);
-    dll.display();
-    
 }
